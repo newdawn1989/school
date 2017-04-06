ac970fbaba31f5fa1156a7837659303049db2dd2
Nathan Lile <newdawn1989@gmail.com>
2017-04-06 15:34:51 -0600

    Updating kernals.c

--- kernels.c
+++ kernels.c
@@ -24,11 +24,21 @@ char naive_rotate_descr[] = "naive_rotate: Naive baseline implementation";
 void naive_rotate(int dim, pixel *src, pixel *dst) 
 {
     int i, j;
-    for (i = 0; i < dim; i++){
-        for (j = 0; j < dim; j++){
+    for (i = 0; i < dim; i+=2){
+        for (j = 0; j < dim; j+=2){
             dst[RIDX(dim-1-j, i, dim)].red   = src[RIDX(i, j, dim)].red;
             dst[RIDX(dim-1-j, i, dim)].green = src[RIDX(i, j, dim)].green;
             dst[RIDX(dim-1-j, i, dim)].blue  = src[RIDX(i, j, dim)].blue;
+            dst[RIDX(dim-1-j-1, i, dim)].red   = src[RIDX(i, j+1, dim)].red;
+            dst[RIDX(dim-1-j-1, i, dim)].green = src[RIDX(i, j+1, dim)].green;
+            dst[RIDX(dim-1-j-1, i, dim)].blue  = src[RIDX(i, j+1, dim)].blue;
+
+            dst[RIDX(dim-1-j, i+1, dim)].red   = src[RIDX(i+1, j, dim)].red;
+            dst[RIDX(dim-1-j, i+1, dim)].green = src[RIDX(i+1, j, dim)].green;
+            dst[RIDX(dim-1-j, i+1, dim)].blue  = src[RIDX(i+1, j, dim)].blue;
+            dst[RIDX(dim-1-j-1, i+1, dim)].red   = src[RIDX(i+1, j+1, dim)].red;
+            dst[RIDX(dim-1-j-1, i+1, dim)].green = src[RIDX(i+1, j+1, dim)].green;
+            dst[RIDX(dim-1-j-1, i+1, dim)].blue  = src[RIDX(i+1, j+1, dim)].blue;
         
         }
     }
@@ -42,64 +52,268 @@ void naive_rotate(int dim, pixel *src, pixel *dst)
  * Your different versions of the rotate kernel go here
  ******************************************************/
 
-char attempt_1_rotate_descr[] = "Attempt 1";
+char attempt_1_rotate_descr[] = "Attempt 1===working with loop unrolling";
 void attempt_1_rotate(int dim, pixel *src, pixel *dst) 
 {
     
-    
-        int i, j;
-    for (i = 0; i < dim; i+=4){
-        for (j = 0; j < dim; j+=4){
-            dst[RIDX(dim-1-0, i, dim)]   = src[RIDX(i, 0, dim)];
-            dst[RIDX(dim-1-1, i, dim)]   = src[RIDX(i, 1, dim)];
-            dst[RIDX(dim-1-2, i, dim)]   = src[RIDX(i, 2, dim)];
-            dst[RIDX(dim-1-3, i, dim)]   = src[RIDX(i, 3, dim)];
+  int block_1 = 16;
+  int block_2 = 16;   
+ int i, j, i1, j1;
+  for(i1 = 0; i1 < dim; i1 += block_1) {
+    for(j1 = 0; j1< dim; j1 += block_2){
+      for (i = i1; i < i1+block_1; i+=2){
+        for (j = j1; j < j1+block_2; j+=2){
+            dst[RIDX(dim-1-j, i, dim)] = src[RIDX(i, j, dim)];
+            dst[RIDX(dim-1-j-1, i, dim)]   = src[RIDX(i, j+1, dim)];
+            ///////////////////////// taking care of i+1 ////////////////////////////////////////
+            dst[RIDX(dim-1-j, i+1, dim)]  = src[RIDX(i+1, j, dim)];
+            dst[RIDX(dim-1-j-1, i+1, dim)]   = src[RIDX(i+1, j+1, dim)];
+            
+        
+        }
+      }
+    }
+  }
+}
+
+#define work(x) dst[RIDX(dim-1-j, i+(x), dim)] = src[RIDX(i+(x), j, dim)];
+char attempt_2_rotate_descr[] = "Attempt 2===========function call inside loop with static incrimenting of inner variables";
+void attempt_2_rotate(int dim, pixel *src, pixel *dst) 
+{
+   int i, j;
+
+    for (i = 0; i < dim; i += 16) 
+        for (j = 0; j < dim; j++) {
+            work(0);work(1);work(2);work(3);
+            work(4);work(5);work(6);work(7);
+            work(8);work(9);work(10);work(11);
+            work(12);work(13);work(14);work(15);
+
+        }
+}
+/*
+NOTES:
+we do the work of the inner for loop in an external function and call in the loop. Function calls are faster then iteration work
+*/
+char attempt_3_rotate_descr[] = "Attempt 3===========using pointers for src and dst";
+void attempt_3_rotate(int dim, pixel *src, pixel *dst) 
+{
+   int i, j, dim_local;
+   pixel *src_ptr, *dst_ptr;
+   dim_local = dim;
+    for (i = 0; i < dim_local; i += 32){ 
+        for (j = dim_local-1; j >=0; j--) {
+        dst_ptr = dst+RIDX(dim-1-j, i, dim_local);
+        src_ptr = src+RIDX(i,j, dim_local);  
+        *(dst_ptr) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+1) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+2) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+3) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+4) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+5) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+6) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+7) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+8) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+9) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+10) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+11) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+12) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+13) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+14) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+15) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+16) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+17) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+18) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+19) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+20) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+21) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+22) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+23) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+24) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+25) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+26) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+27) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+28) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+29) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+30) = *(src_ptr);
+        src_ptr+=dim_local;
+        *(dst_ptr+31) = *(src_ptr);
+        }
+   }
+}
+/*
+NOTES:
+32 fold-loop unrolling and pointers for src and dst
+swaped i and j to get a slight bump in speed
+let inner loop decriment rather then incriment for slight increase in speed
+TRY LOCAL DIM VARIABLE
+*/
+/*
+LOOP BLOCKING:
+
+*/
+char attempt_4_rotate_descr[] = "Attempt 4===========loop blocking and loop unrolling using pointers to src and dst";
+void attempt_4_rotate(int dim, pixel *src, pixel *dst) 
+{
+    //Block size to tune
+  int block_1 = 8;
+  int block_2 = 8;
+  int i,j,i1,j1, dim_local;
+pixel *src_ptr, *dst_ptr;
+  dim_local = dim;
+  for(i1 = 0; i1 < dim_local; i1 += block_1) {
+    for(j1 = 0; j1< dim_local; j1 += block_2) {
+            for(i = i1; i < i1 + block_1; i+=8){
+                for(j = j1; j < j1 + block_2; j++){
+                            dst_ptr = dst+RIDX(dim-1-j, i, dim_local);
+                            src_ptr = src+RIDX(i,j, dim_local);  
+                            *(dst_ptr) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+1) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+2) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+3) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+4) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+5) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+6) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+7) = *(src_ptr);
+/* 
+                             src_ptr+=dim_local;
+                            *(dst_ptr+8) = *(src_ptr);
+                             src_ptr+=dim_local;
+                            *(dst_ptr+9) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+10) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+11) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+12) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+13) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+14) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+15) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+16) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+17) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+18) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+19) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+20) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+21) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+22) = *(src_ptr);
+                             src_ptr+=dim_local;
+                            *(dst_ptr+23) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+24) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+25) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+26) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+27) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+28) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+29) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+30) = *(src_ptr);
+                            src_ptr+=dim_local;
+                            *(dst_ptr+31) = *(src_ptr);
+                            */
+                }
+            }
         }
     }
 
 }
-
+/*
+NOTES:
+You should think of I[RIDX(i,j,n)] as equivalent to I[i][j].
+Accessing the array in A[i][j] order, taking advantage of spatial locality why so slow?
+*/
 char playGround_rotate_descr[] = "PLAYGROUND======optimize array index cacheing";
 void playGround_rotate(int dim, pixel *src, pixel *dst) 
 {	
     //You should think of I[RIDX(i,j,n)] as equivalent to I[i][j]
-int i, j, j1;
-		int T = 32;
-		for(i = 0; i < dim; i+=T)
-			for(j = 0; j < dim; j+=T)
-				for(j1 = j; j1 < j+T; j1++) {
-					dst[RIDX(dim-1-j1, i+0, dim)] = src[RIDX(i+0, j1, dim)];
-					dst[RIDX(dim-1-j1, i+1, dim)] = src[RIDX(i+1, j1, dim)];
-					dst[RIDX(dim-1-j1, i+2, dim)] = src[RIDX(i+2, j1, dim)];
-					dst[RIDX(dim-1-j1, i+3, dim)] = src[RIDX(i+3, j1, dim)];
-					dst[RIDX(dim-1-j1, i+4, dim)] = src[RIDX(i+4, j1, dim)];
-					dst[RIDX(dim-1-j1, i+5, dim)] = src[RIDX(i+5, j1, dim)];
-					dst[RIDX(dim-1-j1, i+6, dim)] = src[RIDX(i+6, j1, dim)];
-					dst[RIDX(dim-1-j1, i+7, dim)] = src[RIDX(i+7, j1, dim)];
-					dst[RIDX(dim-1-j1, i+8, dim)] = src[RIDX(i+8, j1, dim)];
-					dst[RIDX(dim-1-j1, i+9, dim)] = src[RIDX(i+9, j1, dim)];
-					dst[RIDX(dim-1-j1, i+10, dim)] = src[RIDX(i+10, j1, dim)];
-					dst[RIDX(dim-1-j1, i+11, dim)] = src[RIDX(i+11, j1, dim)];
-					dst[RIDX(dim-1-j1, i+12, dim)] = src[RIDX(i+12, j1, dim)];
-					dst[RIDX(dim-1-j1, i+13, dim)] = src[RIDX(i+13, j1, dim)];
-					dst[RIDX(dim-1-j1, i+14, dim)] = src[RIDX(i+14, j1, dim)];
-					dst[RIDX(dim-1-j1, i+15, dim)] = src[RIDX(i+15, j1, dim)];
-					dst[RIDX(dim-1-j1, i+16, dim)] = src[RIDX(i+16, j1, dim)];
-					dst[RIDX(dim-1-j1, i+17, dim)] = src[RIDX(i+17, j1, dim)];
-					dst[RIDX(dim-1-j1, i+18, dim)] = src[RIDX(i+18, j1, dim)];
-					dst[RIDX(dim-1-j1, i+19, dim)] = src[RIDX(i+19, j1, dim)];
-					dst[RIDX(dim-1-j1, i+20, dim)] = src[RIDX(i+20, j1, dim)];
-					dst[RIDX(dim-1-j1, i+21, dim)] = src[RIDX(i+21, j1, dim)];
-					dst[RIDX(dim-1-j1, i+22, dim)] = src[RIDX(i+22, j1, dim)];
-					dst[RIDX(dim-1-j1, i+23, dim)] = src[RIDX(i+23, j1, dim)];
-					dst[RIDX(dim-1-j1, i+24, dim)] = src[RIDX(i+24, j1, dim)];
-					dst[RIDX(dim-1-j1, i+25, dim)] = src[RIDX(i+25, j1, dim)];
-					dst[RIDX(dim-1-j1, i+26, dim)] = src[RIDX(i+26, j1, dim)];
-					dst[RIDX(dim-1-j1, i+27, dim)] = src[RIDX(i+27, j1, dim)];
-					dst[RIDX(dim-1-j1, i+28, dim)] = src[RIDX(i+28, j1, dim)];
-					dst[RIDX(dim-1-j1, i+29, dim)] = src[RIDX(i+29, j1, dim)];
-					dst[RIDX(dim-1-j1, i+30, dim)] = src[RIDX(i+30, j1, dim)];
-					dst[RIDX(dim-1-j1, i+31, dim)] = src[RIDX(i+31, j1, dim)];
+int i, j, j_1;
+		int size = 32;
+		for(i = 0; i < dim; i+=size)
+			for(j = 0; j < dim; j+=size)
+				for(j_1 = j; j_1 < j+size; j_1++) {
+					dst[RIDX(dim-1-j_1, i+0, dim)] = src[RIDX(i+0, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+1, dim)] = src[RIDX(i+1, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+2, dim)] = src[RIDX(i+2, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+3, dim)] = src[RIDX(i+3, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+4, dim)] = src[RIDX(i+4, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+5, dim)] = src[RIDX(i+5, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+6, dim)] = src[RIDX(i+6, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+7, dim)] = src[RIDX(i+7, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+8, dim)] = src[RIDX(i+8, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+9, dim)] = src[RIDX(i+9, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+10, dim)] = src[RIDX(i+10, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+11, dim)] = src[RIDX(i+11, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+12, dim)] = src[RIDX(i+12, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+13, dim)] = src[RIDX(i+13, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+14, dim)] = src[RIDX(i+14, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+15, dim)] = src[RIDX(i+15, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+16, dim)] = src[RIDX(i+16, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+17, dim)] = src[RIDX(i+17, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+18, dim)] = src[RIDX(i+18, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+19, dim)] = src[RIDX(i+19, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+20, dim)] = src[RIDX(i+20, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+21, dim)] = src[RIDX(i+21, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+22, dim)] = src[RIDX(i+22, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+23, dim)] = src[RIDX(i+23, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+24, dim)] = src[RIDX(i+24, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+25, dim)] = src[RIDX(i+25, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+26, dim)] = src[RIDX(i+26, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+27, dim)] = src[RIDX(i+27, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+28, dim)] = src[RIDX(i+28, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+29, dim)] = src[RIDX(i+29, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+30, dim)] = src[RIDX(i+30, j_1, dim)];
+					dst[RIDX(dim-1-j_1, i+31, dim)] = src[RIDX(i+31, j_1, dim)];
 				}
 }
 
@@ -110,7 +324,7 @@ STEPS
 1) optimize for loops
 
    - performing loop unrolling for the inner loop, rather then doing an 32 factor unrollilng.
-   - sets static variable for the dimension of the unrolliing so it is not recalcualted over and over T = 32;
+   - sets static variable for the dimension of the unrolliing so it is not recalcualted over and over size = 32;
    - reduced the dst[RIDX(dim-1-j1, i+0, dim)] = src[RIDX(i+0, j1, dim)].red, green and blue to single statement.
    - 3rd for loop to decriment the j variable by the same amount the i variable is being incrimented by
         i) this allows for more efficitne caching of the array index iinformation in memory??
@@ -119,8 +333,11 @@ not 100% sure why this works so damn well, ask CA's
 char rotate_descr[] = "rotate: Current working version";
 void rotate(int dim, pixel *src, pixel *dst) 
 {
-    playGround_rotate(dim, src, dst);
+    //playGround_rotate(dim, src, dst);
     attempt_1_rotate(dim, src, dst);
+    //attempt_2_rotate(dim, src, dst);
+    attempt_3_rotate(dim, src, dst);
+    attempt_4_rotate(dim, src, dst);
 }
 
 /*********************************************************************
@@ -135,8 +352,13 @@ void register_rotate_functions()
 {
     add_rotate_function(&naive_rotate, naive_rotate_descr);   
     add_rotate_function(&rotate, rotate_descr);   
-    //add_rotate_function(&attempt_1_rotate, attempt_1_rotate_descr);
-    add_rotate_function(&playGround_rotate, playGround_rotate_descr);
+    add_rotate_function(&attempt_1_rotate, attempt_1_rotate_descr);
+    //add_rotate_function(&attempt_2_rotate, attempt_2_rotate_descr);
+    add_rotate_function(&attempt_3_rotate, attempt_3_rotate_descr);
+    add_rotate_function(&attempt_4_rotate, attempt_4_rotate_descr);
+    //add_rotate_function(&playGround_rotate, playGround_rotate_descr);
+    
+    
      
 }
 
@@ -194,8 +416,8 @@ static pixel avg(int dim, int i, int j, pixel *src)
     initialize_pixel_sum(&sum);
     for(ii = max(i-1, 0); ii <= min(i+1, dim-1); ii++) 
 	    for(jj = max(j-1, 0); jj <= min(j+1, dim-1); jj++) 
-	    accumulate_sum(&sum, src[RIDX(ii, jj, dim)]);
-    assign_sum_to_pixel(&current_pixel, sum);
+	      accumulate_sum(&sum, src[RIDX(ii, jj, dim)]);
+        assign_sum_to_pixel(&current_pixel, sum);
     return current_pixel;
 }
  /* naive_smooth - The naive baseline version of smooth */
@@ -241,7 +463,7 @@ void attempt_1_smooth(int dim, pixel *src, pixel *dst)
         dst[RIDX(i, j, dim)] = avg(dim, i, j, src);
         
 }
-char attempt_2_smooth_descr[] = "ATTEMPT 1";
+char attempt_2_smooth_descr[] = "ATTEMPT 2";
 void attempt_2_smooth(int dim, pixel *src, pixel *dst) 
 {
   int i, j, tmp, tmp_i;
@@ -335,11 +557,11 @@ square tiles and smoothing one tile at a time. This way we achieve better cache 
         ii) upper left X
         iii) lower right X
         iv) lower left X
-        v)upper  edge
-        vi) lower edge
-        vii)  left edge
-        viii) right edge
-        ix) cenfer
+        v)upper  edge X
+        vi) lower edge X
+        vii)  left edge X
+        viii) right edge X
+        ix) cenfer X
  ******************************************************/
 
  /* smooth - Your current working version of smooth. 
@@ -348,8 +570,8 @@ square tiles and smoothing one tile at a time. This way we achieve better cache 
 char smooth_descr[] = "smooth: Current working version";
 void smooth(int dim, pixel *src, pixel *dst) 
 {
-    naive_smooth(dim, src, dst);
-    attempt_1_smooth(dim, src, dst);
+    //naive_smooth(dim, src, dst);
+    //attempt_1_smooth(dim, src, dst);
     attempt_2_smooth(dim, src, dst);
 }
 
@@ -363,9 +585,9 @@ void smooth(int dim, pixel *src, pixel *dst)
  *********************************************************************/
 
 void register_smooth_functions() {
-    add_smooth_function(&smooth, smooth_descr);
+    //add_smooth_function(&smooth, smooth_descr);
     add_smooth_function(&naive_smooth, naive_smooth_descr);
-    add_smooth_function(&attempt_1_smooth, attempt_1_smooth_descr);
+    //add_smooth_function(&attempt_1_smooth, attempt_1_smooth_descr);
     add_smooth_function(&attempt_2_smooth, attempt_2_smooth_descr);
     /* ... Register additional test functions here */
 }
